57009a89a832b0717bf2864e3391e17321afd0c3
Brian Reynolds <brikrey@gmail.com>
2017-12-07 12:18:27 -0500

    added all listeners and tested inputs

--- atm.js
+++ atm.js
@@ -1,3 +1,84 @@
 $(document).ready(function(){
-    $('body').css('background-color','red')
+    //$('body').css('background-color','red')
+
+    $(`#checking .deposit`).on({
+        click: function(){
+        //click listener to the checking account's "Deposit" button
+        //When you click the element it should console.log("hello")  
+        //console.log("hello")
+
+        //On clicking checking "Deposit", it should get the user input and console.log it
+        var checkingDeposit = $(`#checking .input`).val()
+        console.log(checkingDeposit)
+        }
+    })
+
+    $(`#savings .deposit`).on({
+        click: function(){
+        //click listener to the savings account's "Deposit" button
+        //When you click the element it should console.log("hello")  
+        //console.log("hello")
+
+        //On clicking savings "Deposit", it should get the user input and console.log it
+        var savingsDeposit = $(`#savings .input`).val()
+        console.log(savingsDeposit)        
+        }
+    })
+
+    $(`#checking .withdraw`).on({
+        click: function(){
+        //click listener to the chercking account's "Withdraw" button
+        //When you click the element it should console.log("hello")  
+        //console.log("hello")
+
+        //On clicking checking "Withdraw", it should get the user input and console.log it
+        var checkingWithdraw = $(`#checking .input`).val()
+        console.log(checkingWithdraw)  
+        }
+    })
+
+    $(`#savings .withdraw`).on({
+        click: function(){
+        //click listener to the savings account's "Withdraw" button
+        //When you click the element it should console.log("hello")  
+        //console.log("hello")
+
+        //On clicking savings "Withdraw", it should get the user input and console.log it
+        var savingsWithdraw = $(`#savings .input`).val()
+        console.log(savingsWithdraw)  
+        }
+    })
+
+    $(`#checking .balance`).on({
+        click: function(){
+        //click listener to the checking account's "balance"
+        //When you click the element it should console.log("hello")  
+        //console.log("hello")
+        console.log("checking balance")
+        }
+    })
+
+    $(`#savings .balance`).on({
+        click: function(){
+        //click listener to the savings account's "balance"
+        //When you click the elemewnt it should console.log("hello")  
+        //console.log("hello")
+        }
+    })
+
+    $(`#checking .input`).on({
+        focus: function(){
+        //click listener to the checking account's input field
+        //When you click the element it should console.log("hello")  
+        //console.log("hello")
+        }
+    })
+
+    $(`#savings .input`).on({
+        focus: function(){
+        //click listener to the savings account's input field
+        //When you click the element it should console.log("hello")  
+        //console.log("hello")      
+        }
+    })
 });
