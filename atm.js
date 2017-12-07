3b8d004dc11113e059adbd2ee3b174c9906d129c
Brian Reynolds <brikrey@gmail.com>
2017-12-07 13:21:43 -0500

    added ability to manage balances

--- atm.js
+++ atm.js
@@ -1,3 +1,6 @@
+var checkingBalance = 0
+var savingsBalance = 0
+
 $(document).ready(function(){
     //$('body').css('background-color','red')
 
@@ -6,11 +9,19 @@ $(document).ready(function(){
         //click listener to the checking account's "Deposit" button
         //When you click the element it should console.log("hello")  
         //console.log("hello")
+        
+        //On clicking checking "Deposit", it should get the user input and console.log it       
+        //checkingBalance = $(`#checking .input`).val()
+        //console.log(checkingBalance)
 
-        //On clicking checking "Deposit", it should get the user input and console.log it
-        var checkingDeposit = $(`#checking .input`).val()
-        //logs inut gotten
-        console.log(checkingDeposit)
+        //On clicking checking "Deposit", it should update balance with input
+        checkingBalance = checkingBalance + parseInt($(`#checking .input`).val())
+        $(`#checking .balance`).val(checkingBalance)
+        console.log(checkingBalance)
+        console.log($(`#checking .balance`).val())
+
+        //$(`#checking .balance`).html('div class="balance"> checkingBalance</div>')
+        
         }
     })
 
@@ -19,24 +30,34 @@ $(document).ready(function(){
         //click listener to the savings account's "Deposit" button
         //When you click the element it should console.log("hello")  
         //console.log("hello")
+        
+        //On clicking savings "Deposit", it should get the user input and console.log it       
+        //savingsBalance = $(`#savings .input`).val()
+        //console.log(savingsBalance)
 
-        //On clicking savings "Deposit", it should get the user input and console.log it
-        var savingsDeposit = $(`#savings .input`).val()
-        //logs inut gotten
-        console.log(savingsDeposit)        
+        //On clicking savings "Deposit", it should update balance with input
+        savingsBalance = savingsBalance + parseInt($(`#savings .input`).val())   
+        $(`#savings .balance`).val(savingsBalance)
+        console.log(savingsBalance)
+        console.log($(`#savings .balance`).val())
         }
     })
 
     $(`#checking .withdraw`).on({
         click: function(){
-        //click listener to the chercking account's "Withdraw" button
+        //click listener to the checking account's "Withdraw" button
         //When you click the element it should console.log("hello")  
         //console.log("hello")
+        
+        //On clicking checking "Withdraw", it should get the user input and console.log it       
+        //checkingBalance = $(`#checking .input`).val()
+        //console.log(checkingBalance)
 
-        //On clicking checking "Withdraw", it should get the user input and console.log it
-        var checkingWithdraw = $(`#checking .input`).val()        
-        //logs inut gotten
-        console.log(checkingWithdraw)  
+        //On clicking checking "Withdraw", it should update balance with input
+        checkingBalance = checkingBalance - parseInt($(`#checking .input`).val())
+        $(`#checking .balance`).val(checkingBalance)
+        console.log(checkingBalance)
+        console.log($(`#checking .balance`).val())
         }
     })
 
@@ -45,44 +66,49 @@ $(document).ready(function(){
         //click listener to the savings account's "Withdraw" button
         //When you click the element it should console.log("hello")  
         //console.log("hello")
+        
+        //On clicking savings "Withdraw", it should get the user input and console.log it       
+        //savingsBalance = $(`#savings .input`).val()
+        //console.log(savingsBalance)
 
-        //On clicking savings "Withdraw", it should get the user input and console.log it
-        var savingsWithdraw = $(`#savings .input`).val()
-        //logs inut gotten
-        console.log(savingsWithdraw)
+        //On clicking savings "Withdraw", it should update balance with input
+        savingsBalance = savingsBalance - parseInt($(`#savings .input`).val())   
+        $(`#savings .balance`).val(savingsBalance)
+        console.log(savingsBalance)
+        console.log($(`#savings .balance`).val())
         }
     })
 
-    $(`#checking .balance`).on({
-        click: function(){
+    // $(`#checking .balance`).on({
+    //     click: function(){
         //click listener to the checking account's "balance"
         //When you click the element it should console.log("hello")  
         //console.log("hello")
-        console.log("checking balance")
-        }
-    })
+        //console.log("hello")
+    //     }
+    // })
 
-    $(`#savings .balance`).on({
-        click: function(){
+    // $(`#savings .balance`).on({
+    //     click: function(){
         //click listener to the savings account's "balance"
         //When you click the elemewnt it should console.log("hello")  
         //console.log("hello")
-        }
-    })
+    //     }
+    // })
 
-    $(`#checking .input`).on({
-        focus: function(){
+    // $(`#checking .input`).on({
+    //     focus: function(){
         //click listener to the checking account's input field
         //When you click the element it should console.log("hello")  
         //console.log("hello")
-        }
-    })
+    //     }
+    // })
 
-    $(`#savings .input`).on({
-        focus: function(){
+    // $(`#savings .input`).on({
+    //     focus: function(){
         //click listener to the savings account's input field
         //When you click the element it should console.log("hello")  
         //console.log("hello")      
-        }
-    })
+    //     }
+    // })
 });
